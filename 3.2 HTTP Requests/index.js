de459e26fd06df7cf736980e1e413cef6f68045d
Hatef Eidi <hatef.121.h@gmail.com>
2025-04-12 14:20:34 +0100

    Added page

--- 3.2 HTTP Requests/index.js
+++ 3.2 HTTP Requests/index.js
@@ -6,6 +6,12 @@ const port=3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}.`);
 });
+app.get("/", (req, res) => {
+  res.send(`<h1> Hello, this is my first express server </h1>`);
+});
+app.get("/contact", (req, res) => {
+    res.send(`<h1> Hello, this is my contact page </h1>`);
+    });
 
 app.get("/about", (req, res) => {
 res.send(`<h1> Hello, this is about me </h1>`);
